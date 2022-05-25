618a957a36d9db3692d84d2dd3a3f31c33398642
Q <rqjones@asu.edu>
2022-05-24 18:46:09 -0700

    new functions
    submitTransaction()
    checkTransactions()
    confirmTransaction()

--- Multi-Sig-Key.sol
+++ Multi-Sig-Key.sol
@@ -4,12 +4,26 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 abstract contract Treasury is ERC1155{
     uint KEYS =0;
+    uint TotalTransactions=0;
     uint i;
     mapping(uint => KeyListings) keys;
-
+    mapping(uint => VoteOnTransaction) vote;
+    mapping(uint => MultiSigTransaction) MSTrans;
+    //list Keys
     struct KeyListings{
         uint count;
         bool exist;
+    }
+    //cast vote for transaction
+    struct VoteOnTransaction{
+        uint Key;
+        bool status;
+    }
+    struct MultiSigTransaction{
+        uint ammount;
+        address toAddress;
+        string topic;
+        string messege;
     }
     //Launch Contract and Keys
     constructor(uint _totalKeys, uint _VotesNeededToPass,string memory URI)ERC1155(URI){
@@ -19,27 +33,37 @@ abstract contract Treasury is ERC1155{
             keys[i] = KeyListings(i,true);
             _mint(msg.sender, i, 1, "");
         }
-        
     }
     //modifier checks to see if user has a key before executing the function
     modifier CheckKeys{
-        require(checkTokens(msg.sender) == true);
+        (bool keyStatus,uint keyNumb) = checkTokens(msg.sender);
+        require(keyStatus == true);
         _;
     }
     //internal function checks to see if user has key
-    function checkTokens(address _user) internal returns(bool){
+    function checkTokens(address _user) internal returns(bool,uint){
         uint i;
         for(i=0; i <= KEYS; i++){
             if(balanceOf(_user,i) > 0){
-                return true;
+                return (true,i);
             } else if(i == KEYS){
-                return false;
+                return (false,i);
             }
         }       
     }
-
-    function submitTransaction() public CheckKeys returns(bool){}
-    function confrimTransaction() public CheckKeys returns(bool){}
+    //view previouse & pending transactions
+    function viewTransaction(uint _transactionNumb) public CheckKeys returns(uint,address,string memory,string memory){
+        return (MSTrans[_transactionNumb].ammount ,MSTrans[_transactionNumb].toAddress ,MSTrans[_transactionNumb].topic ,MSTrans[_transactionNumb].messege);
+    }
+    //view transactions in contract
+    function checkTransactions() internal returns(uint,uint){}
+    //make a submittion to move funds to the contract
+    function submitTransaction(uint _ammount, address _toAddress,string memory _topic,string memory _messege) public CheckKeys returns(bool){
+        MSTrans[TotalTransactions] = MultiSigTransaction(_ammount,_toAddress,_topic,_messege);
+        TotalTransactions++;
+        return true;
+    }
+    function confirmTransaction(uint _TransactionNumber, bool _vote) public CheckKeys returns(bool,bool){}
     function executeTransaction() public CheckKeys returns(bool){}
     function revokeConfirmation() public CheckKeys returns(bool){}
 }
