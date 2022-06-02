7ac9deda115209371cf41c8dfb89a311200a509c
Q <rqjones@asu.edu>
2022-06-02 10:08:38 -0700

    Update testTreasury.js

--- test/testTreasury.js
+++ test/testTreasury.js
@@ -1,11 +1,12 @@
 const MultiSigTreasury = artifacts.require("MultiSigTreasury");
 
 contract(MultiSigTreasury, () => {
+    let treasury = null;
+
     before( async() => {
-        var treasury = await MultiSigTreasury.deployed(5,3,"test 1");
+        treasury = await MultiSigTreasury.deployed(5,3,"test 1");
     });
     it("Launch MultiSigTreasury contract", async() =>  {
-        console.log("check address");
         assert(await treasury.address !== '', "Coinbank Address Expected");
     })
 })
