1bf03fd9c2638470481a7b618389e2e919fa2a54
Q <rqjones@asu.edu>
2022-07-04 20:33:07 -0700

    test scripts

--- test/testTreasury.js
+++ test/testTreasury.js
@@ -1,12 +1,28 @@
 const MultiSigTreasury = artifacts.require("MultiSigTreasury");
 
+//Unit Testing on Multi Sig Contract
 contract(MultiSigTreasury, () => {
     let treasury = null;
 
     before( async() => {
-        treasury = await MultiSigTreasury.deployed(5,3,"test 1");
+        treasury = await MultiSigTreasury.deployed(3,2,"test 1");
+        partyA = accounts[0];
+        partyB = accounts[1];
+        partyC = accounts[1];
     });
     it("Launch MultiSigTreasury contract", async() =>  {
-        assert(await treasury.address !== '', "Coinbank Address Expected");
+        assert(await treasury.address !== '', "Multi Sig Launched ");
     })
+    it("submitProposal()", async() =>  {
+        uint = [];
+        address = [];
+        string = [];
+        assert(await treasury.submitProposal(uint,address,string) == true, "Proposal submitted ");
+    })
+
+    //it("confirmTransaction()", async() =>  { })
+    //it("checkVote()", async() =>  { })
+    //it("revokeConfirmation()", async() =>  { })
+    //it("checkVote()", async() =>  { })
+    //it("Complete Vote", async() =>  { })
 })
