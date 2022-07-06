a17de7cafd3a62fa0b70abf56f5bba430acfcb6a
Q <rqjones@asu.edu>
2022-07-06 08:49:44 -0700

    test governance vote script

--- test/testTreasury.js
+++ test/testTreasury.js
@@ -10,22 +10,34 @@ contract(MultiSigTreasury, (accounts) => {
         const partyA = accounts[0];
         const partyB = accounts[1];
 
+        treasury.safeTransferFrom(partyA, partyB, 1, 1, "0x0")
         web3.eth.sendTransaction({to:treasury.address, from:accounts[0], value:web3.utils.toWei("1", "ether")})
     });
     it("Launch MultiSigTreasury contract", async() =>  {
         assert(await treasury.address !== '', "Multi Sig Launched ");
     })
-    it("submitProposal()", async() =>  {
+    it("execute submitProposal()", async() =>  {
         var uint = [0];
-        var address = ["0x0000000000000000000000000000000000000000","0x0000000000000000000000000000000000000000"];
+        var address = ["0x0000000000000000000000000000000000000000"];
         var string = ["Test blank Transaction","MSG",""];
 
-        const result = treasury.submitProposal(uint,address,string);
-        assert(result === true, "Proposal submitted");
+        const result = await treasury.submitProposal(uint,address,string);
+        assert.equal(result.receipt.status, true, "Proposal submitted");
     })
-    //it("confirmTransaction()", async() =>  { })
-    //it("revokeConfirmation()", async() =>  { })
-    //it("Complete Vote", async() =>  { })
+    it("confirmTransaction()", async() =>  { 
+        const result = await treasury.confirmTransaction(0, true,0, {from: accounts[0]})
+        assert.equal(result.receipt.status, true, "Proposal submitted");
+    })
+    it("revokeConfirmation()", async() =>  { 
+        const result = await treasury.revokeConfirmation(0,0, {from: accounts[0]})
+        assert.equal(result.receipt.status, true, "Proposal submitted");
+    })
+    it("Finnish Vote()", async() =>  { 
+        const confirm = await treasury.confirmTransaction(0, true,0 , {from: accounts[0]})
+        const result = await treasury.confirmTransaction(0, true,1, {from: accounts[1]})
+        //console.log(result.receipt);
+        assert.equal(result.receipt.status, true, "Proposal submitted");
+    })
 })
 
 //XDC Tranaction Testing on Multi Sig Contract
